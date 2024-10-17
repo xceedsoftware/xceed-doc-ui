9eeccd60891fbdffee81262f004014cb09862817
vargasrxceed <vargasr@xceed.com>
2024-10-17 09:21:22 -0400

    updating at runtime

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Xceed UI Technologies for .NET diodottt',
+  title: 'Xceed UI Technologies for .NET',
   tagline: 'Xceed UI Technologies for .NET documentation',
   favicon: 'img/logox.ico',
 
